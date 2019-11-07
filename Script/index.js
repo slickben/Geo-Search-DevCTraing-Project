51c4828a44c334cc15b3956ac3cde534e29d2b88
benson momodu <benson.isaac.momodu@gmail.com>
2019-11-07 18:44:32 +0100

    get current location display weather info

---
+++ Script/index.js
@@ -0,0 +1,95 @@
+//Dom Elements
+const  locationName = document.getElementById('locationName')
+const  Currentweather = document.getElementById('weather')
+const showLocalCondition = document.getElementById('local-condition')
+weather
+
+
+
+//UI  class
+class UI {
+    constructor(CurrentWeatherData) {
+        this.weatherData = CurrentWeatherData
+    }
+    static showNameOfLocation = ({name}) => {
+        locationName.innerText = name
+    }
+    static showCurrentWeether = ({weather}) => {
+        console.log(weather[0].main)
+        Currentweather.innerText = weather[0].main
+    }
+    static showLocalCondition = ({main, wind}) => {
+        let localCondition = [
+            {
+                name: "temprature",
+                value: main.temp
+            },
+            {
+                name: "wind speed",
+                value: wind.speed
+            },
+            {
+                name: "humidity",
+                value: main.humidity
+            }
+        ];
+
+        const symbol = (type) => {
+            if(type === "temprature"){
+                return '\xB0C.'
+            }else if(type === "wind speed") {
+                return 'm/s'
+            }else if(type === "humidity") {
+                return '%'
+            }
+        }
+
+        showLocalCondition.innerHTML = localCondition.map( (val, index) => {
+            return `
+                <li>
+                    <div class="box">
+                        <h1>${val.value}</h1>
+                        <span>${symbol(val.name)}</span>
+                    </div>
+                    <p>${val.name}</p>
+                </li>
+            `
+        })
+    }
+}
+
+
+//get current location 
+const getCurrentLocation = () => {
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition( (position) => {
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            getLocationWeatherData(pos.lat, pos.lng,);
+        })
+    }
+}
+
+//get weather data
+const getLocationWeatherData = (lat, lng,) => {
+    console.log(lat, lng )
+    let key = '3fef7f09e3d16a2d67ba169848bd7e6f';
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lng+ '&appid=' + key)
+    .then( (responce) => {
+        return responce.json()
+    })
+    .then( (data) => {
+        console.log(data)
+        UI.showNameOfLocation(data)
+        UI.showCurrentWeether(data)
+        UI.showLocalCondition(data)
+    })
+}
+
+getCurrentLocation()
+
+
+
+
